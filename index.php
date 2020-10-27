9c3fe7d18c54b35f8d100bcaf32dd59d4dd9ecb2
小正 <352497398@qq.com>
2020-10-27 10:58:18 +0800

    init

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+require "vendor/autoload.php";
+$Test = new Siam\Test();
+$Test->test();
---
+++ src/siam/Test.php
@@ -0,0 +1,10 @@
+<?php
+//Test.php文件
+namespace Siam;
+class Test
+{
+    function test()
+    {
+        echo "Form Test -> test()";
+    }
+}
